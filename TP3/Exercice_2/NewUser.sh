8648101c742ea97d44e1cb5b98167231215facc1
g2k4yh6p1-admin <g2k4yh6p1@gmail.com>
2024-11-20 13:37:02 -0500

    diner

--- TP3/Exercice_2/NewUser.sh
+++ TP3/Exercice_2/NewUser.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 # crée un nouveau utilisateur + crée un répertoire pour ce nouvel utilisateur
 if id "tp3" >/dev/null 2>&1; then
-    sudo adduser tp3 --home /home/repoTP3/
-    mkdir /home/repoTP3/DossierCréé
-    sudo chmod ------rw- /home/repoTP3/DossierCréé
-    echo "Bienvenu au TP3" >>  /home/repoTP3/DossierCréé/MessageBienvenue
-    cat /home/repoTP3/DossierCréé/MessageBienvenue
-    echo " Bienvenu tp3 sur ton nouveau poste de travail "
-    echo " votre repertoire de travail ce nomme DossierCréé "
-    echo $(hostname -I)
+    if [-d /home/repoTP3/]; then
+        sudo adduser tp3 --home /home/repoTP3/
+        mkdir /home/repoTP3/DossierCréé
+        sudo chmod ------rw- /home/repoTP3/DossierCréé
+        echo "Bienvenu au TP3" >>  /home/repoTP3/DossierCréé/message.txt
+        cat /home/repoTP3/DossierCréé/message.txt
+        echo " Bienvenu tp3 sur ton nouveau poste de travail " #todo
+        echo " votre repertoire de travail ce nomme DossierCréé "
+        echo $(hostname -I)
+    else
+        echo " ERROR : repo deja exsitant "
+    fi
+   
 
 else 
-    #todo: error message
+    echo " ERROR : tp3 exsite deja "
 fi
 
 
