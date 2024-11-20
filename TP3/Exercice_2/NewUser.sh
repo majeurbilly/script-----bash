d4994152c672bfa11bc5254139c87a092c25821f
g2k4yh6p1-admin <g2k4yh6p1@gmail.com>
2024-11-20 09:03:48 -0500

    else + note

--- TP3/Exercice_2/NewUser.sh
+++ TP3/Exercice_2/NewUser.sh
@@ -4,16 +4,19 @@ if id "tp3" >/dev/null 2>&1; then
     sudo adduser tp3 --home /home/repoTP3/
     mkdir /home/repoTP3/DossierCréé
     sudo chmod ------rw- /home/repoTP3/DossierCréé
-    echo "Bienvenu au TP3" >>  /home/repoTP3/DossierCréé/MessageBienvenue
+    echo "Bienvenu au TP3" >>  /home/repoTP3/DossierCréé/message.txt
     cat /home/repoTP3/DossierCréé/MessageBienvenue
     echo " Bienvenu tp3 sur ton nouveau poste de travail "
     echo " votre repertoire de travail ce nomme DossierCréé "
     echo $(hostname -I)
 
 else 
-    #todo: error message
+    echo " ERROR : tp3 exsite deja "
 fi
-
+# 4. Éviter la création de répertoire déjà existant.
+# 6. Utilisez la commande ip pour trouver l’adresse ip de la carte réseau.
+# Pour exécuter le script, vous devez donner les permissions d’exécution sur le fichier et l’exécuter en utilisant la commande bash.
+# Si vous avez réussi à créer le script et à l exécuter correctement, vous devriez voir afficher le contenu du fichier message.txt dans la sortie de terminal
 
 
 
