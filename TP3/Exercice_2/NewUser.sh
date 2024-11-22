be14823b1a58384f90670e552fb3ccc54a7c3b80
majeurbilly <billy.halle.bh@gmail.com>
2024-11-21 21:56:39 -0500

    im done

--- TP3/Exercice_2/NewUser.sh
+++ TP3/Exercice_2/NewUser.sh
@@ -1,38 +1,21 @@
 #!/bin/bash
-# crée un nouveau utilisateur + crée un répertoire pour ce nouvel utilisateur
-if id "tp3" >/dev/null 2>&1; then
-    if [-d /home/repoTP3/]; then
-        sudo adduser tp3 --home /home/repoTP3/
-        mkdir /home/repoTP3/DossierCréé
-        sudo chmod ------rw- /home/repoTP3/DossierCréé
-        echo "Bienvenu au TP3" >>  /home/repoTP3/DossierCréé/message.txt
-        cat /home/repoTP3/DossierCréé/message.txt
-        echo " Bienvenu tp3 sur ton nouveau poste de travail " #todo
-        echo " votre repertoire de travail ce nomme DossierCréé "
+read -p "Entrez le nom du projet (ex : tp3) : " nom_projet
+read -p "Entrez le nom du dépôt (ex : repoTP3) : " nom_repo 
+read -p "Entrez le nom du dossier à créer (ex : dossierCréé) : " nom_dossier
+
+if id "$nom_projet" >/dev/null 2>&1; then
+    if [ -d /home/$nom_repo/ ]; then
+        sudo adduser $nom_projet --home /home/$nom_repo/
+        mkdir /home/$nom_repo/$nom_dossier
+        sudo chmod 770 /home/$nom_repo/$nom_dossier
+        echo "Bienvenu au TP3" >>  /home/$nom_repo/$nom_dossier/message.txt
+        cat /home/$nom_repo/$nom_dossier/message.txt
+        echo " Bienvenu $nom_projet sur ton nouveau poste de travail "
+        echo " votre repertoire de travail ce nomme $nom_dossier "
         echo $(hostname -I)
     else
         echo " ERROR : repo deja exsitant "
     fi
-   
-
 else 
     echo " ERROR : tp3 exsite deja "
 fi
-# 4. Éviter la création de répertoire déjà existant.
-# 6. Utilisez la commande ip pour trouver l’adresse ip de la carte réseau.
-# Pour exécuter le script, vous devez donner les permissions d’exécution sur le fichier et l’exécuter en utilisant la commande bash.
-# Si vous avez réussi à créer le script et à l exécuter correctement, vous devriez voir afficher le contenu du fichier message.txt dans la sortie de terminal
-
-
-
-
-
-
-
-
-
-
-
-
-
-
