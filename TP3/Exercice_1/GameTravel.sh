461121afed3f0f852d4eacb555d1ca6c8b8c4e7b
majeurbilly <billy.halle.bh@gmail.com>
2024-11-24 23:21:48 -0500

    finish dimanche

--- TP3/Exercice_1/GameTravel.sh
+++ TP3/Exercice_1/GameTravel.sh
@@ -1,51 +1,54 @@
 #!/bin/bash
+
+read -r -p "Entrez la distance que vous voulez avoir à la route Ouest : " ROUTE_OUEST
+
 # Vérification si un argument est fourni
-if [[ -z "$1" ]]; then
+if [[ -z "$ROUTE_OUEST" ]]; then
   echo "Erreur : Veuillez fournir une distance pour la route ouest."
   exit 1
 fi
 # Vérification si l'argument est un nombre positif
-if ! [[ "$1" =~ ^[0-9]+$ ]]; then
+if ! [[ "$ROUTE_OUEST" =~ ^[0-9]+$ ]]; then
   echo "Erreur : L'entrée doit être un nombre entier positif."
   exit 1
 fi
-# Vérification pour éviter des valeurs négatives
-if [[ $ROUTE_SUD -lt 0 ]]; then
-  echo "Erreur : La route sud a une distance négative ($ROUTE_SUD km)."
-  exit 1
-fi
-if [[ $ROUTE_EST -lt 0 ]]; then
-  echo "Erreur : La route est a une distance négative ($ROUTE_EST km)."
-  exit 1
-fi
-if [[ $ROUTE_NORD -lt 0 ]]; then
-  echo "Erreur : La route nord a une distance négative ($ROUTE_NORD km)."
-  exit 1
-fi
 
-ROUTE_OUEST=$1
-ROUTE_SUD=$(($ROUTE_OUEST - 200))
-ROUTE_EST=$(($ROUTE_SUD + 300))
-ROUTE_NORD=$(($ROUTE_EST + $ROUTE_SUD))
-NB_KM_ROUTE_RAPIDE=$ROUTE_OUEST
+ROUTE_SUD=$(("$ROUTE_OUEST" - 200))
+ROUTE_EST=$(("$ROUTE_SUD" + 300))
+ROUTE_NORD=$(("$ROUTE_EST" + "$ROUTE_SUD"))
+NB_METRE_ROUTE_RAPIDE="$ROUTE_OUEST"
 STRING_ROUTE_RAPIDE="à l'ouest"
 
 echo "---------------------------"
-echo route sud : "$ROUTE_SUD"
-echo route est : "$ROUTE_EST"
-echo route nord : "$ROUTE_NORD"
-echo route ouest : "$ROUTE_OUEST"
+echo route sud : "$ROUTE_SUD" Mètres
+echo route est : "$ROUTE_EST" Mètres
+echo route nord : "$ROUTE_NORD" Mètres
+echo route ouest : "$ROUTE_OUEST" Mètres
 
-if [[ $ROUTE_SUD -lt $NB_KM_ROUTE_RAPIDE ]]; then
-  NB_KM_ROUTE_RAPIDE="$ROUTE_SUD"
+# Vérification pour éviter des valeurs négatives
+if [[ "$ROUTE_SUD" -lt 0 ]]; then
+  echo "Erreur : La route sud a une distance négative (""$ROUTE_SUD"" mètre)."
+  exit 1
+fi
+if [[ "$ROUTE_EST" -lt 0 ]]; then
+  echo "Erreur : La route est a une distance négative (""$ROUTE_EST"" mètre)."
+  exit 1
+fi
+if [[ "$ROUTE_NORD" -lt 0 ]]; then
+  echo "Erreur : La route nord a une distance négative (""$ROUTE_NORD"" mètre)."
+  exit 1
+fi
+
+if [[ "$ROUTE_SUD" -lt "$NB_METRE_ROUTE_RAPIDE" ]]; then
+  NB_METRE_ROUTE_RAPIDE="$ROUTE_SUD"
   STRING_ROUTE_RAPIDE="au sud"
 
-elif [[ "$ROUTE_EST" -lt "$NB_KM_ROUTE_RAPIDE" ]]; then
-  NB_KM_ROUTE_RAPIDE="$ROUTE_EST"
+elif [[ "$ROUTE_EST" -lt "$NB_METRE_ROUTE_RAPIDE" ]]; then
+  NB_METRE_ROUTE_RAPIDE="$ROUTE_EST"
   STRING_ROUTE_RAPIDE="à l'est"
 
-elif [[ "$ROUTE_NORD" -lt "$NB_KM_ROUTE_RAPIDE" ]]; then
-  NB_KM_ROUTE_RAPIDE="$ROUTE_NORD"
+elif [[ "$ROUTE_NORD" -lt "$NB_METRE_ROUTE_RAPIDE" ]]; then
+  NB_METRE_ROUTE_RAPIDE="$ROUTE_NORD"
   STRING_ROUTE_RAPIDE="au nord"
 fi
-echo "La route la plus courte est ""$STRING_ROUTE_RAPIDE"", avec une distance de ""$NB_KM_ROUTE_RAPIDE"" M."
+echo "La route la plus courte est ""$STRING_ROUTE_RAPIDE"", avec une distance de ""$NB_METRE_ROUTE_RAPIDE"" mètres."
--- TP3/Exercice_2/NewUser.sh
+++ TP3/Exercice_2/NewUser.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
-USER_NAME=$(read -r -p "Entrez le nom du USER (ex : tp3) : ")
-NOM_REPO=$(read -r -p "Entrez le nom du dépôt (ex : repoTP3) : ")
-NOM_DOSSSIER=$(read -r -p "Entrez le nom du dossier à créer (ex : dossierCréé) : ")
+read -r -p "Entrez le nom d'utilisateur (ex : tp3) : " USER_NAME
+read -r -p "Entrez le nom du repositoire (ex : repoTP3) : " NOM_REPO
+read -r -p "Entrez le nom du dossier à créer (ex : dossierCréé) : " NOM_DOSSIER
 
 # Vérification des champs obligatoires
 if [[ -z "$USER_NAME" || -z "$NOM_REPO" || -z "$NOM_DOSSIER" ]]; then
   echo "Erreur : Tous les champs doivent être renseignés."
   exit 1
+
+else
+  echo -e "Nom d'utilisateur : $USER_NAME\nNom du répertoire : $NOM_REPO\nNom du dossier créé : $NOM_DOSSIER"
 fi
 
 if id "$USER_NAME" >/dev/null 2>&1; then
     echo " ERROR : ""$USER_NAME"" exsite deja "
     exit 1
 fi
+
 if [[ -d "/home/""$NOM_REPO""" ]]; then
     echo " ERROR : ""$NOM_REPO"" deja exsitant "
     exit 1
 fi
+
 sudo adduser --home /home/"$NOM_REPO"/ "$USER_NAME"
-mkdir /home/"$NOM_REPO"/"$NOM_DOSSSIER"
-sudo chown "$USER_NAME":"$USER_NAME" /home/"$NOM_REPO"/"$NOM_DOSSSIER"
-sudo chmod 770 /home/"$NOM_REPO"/"$NOM_DOSSSIER"
-echo "Bienvenue au TP3" > /home/"$NOM_REPO"/"$NOM_DOSSSIER"/message.txt
-cat /home/"$NOM_REPO"/"$NOM_DOSSSIER"/message.txt
+mkdir /home/"$NOM_REPO"/"$NOM_DOSSIER"
+sudo chown "$USER_NAME":"$USER_NAME" /home/"$NOM_REPO"/"$NOM_DOSSIER"
+sudo chmod 770 /home/"$NOM_REPO"/"$NOM_DOSSIER"
+echo "Bienvenue au TP3" > /home/"$NOM_REPO"/"$NOM_DOSSIER"/message.txt
+cat /home/"$NOM_REPO"/"$NOM_DOSSIER"/message.txt
+clear
 echo "Bienvenue $USER_NAME sur ton nouveau poste de travail."
-echo "Votre répertoire de travail se nomme $NOM_DOSSSIER."
+echo "Votre répertoire de travail se nomme $NOM_DOSSIER."
 echo "Adresse IP : $(hostname -I)"
